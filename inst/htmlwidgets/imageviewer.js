5ebed591b38bbf9c82d023e5e55a54033c6ebe48
Iakov Pustilnik <xyapus@gmail.com>
2018-01-20 02:34:55 +0300

    fix bar charts

--- inst/htmlwidgets/imageviewer.js
+++ inst/htmlwidgets/imageviewer.js
@@ -6,33 +6,62 @@ HTMLWidgets.widget({
 
   factory: function(el, width, height) {
 
-    // TODO: define shared variables for this instance
+    var barchartSize = 128;
+    var widgetInnerHtml = (id, w, h) => {
+      return `<canvas id="barchartY" width="${barchartSize}" height="${h}"></canvas><!--
+           --><canvas id="image" width="${w}" height="${h}"></canvas><div id="sliders"><!--
+           --><div id="brightness_${id}" class="brightness slider"></div><!--
+           --><div id="contrast_${id}" class="contrast slider"></div><!--
+           --></div><!--
+           --><div class="leftbottom"></div><!--
+           --><div id="outputValues"><!--
+           --><span>X:</span><input type="text" size="10"/>&nbsp;<!--
+           --><span>Y:</span><input type="text" size="10"/>&nbsp;<!--
+           --><span>VALUE:</span><input type="text" size="10"/></div><!--
+           --><canvas id="barchartX" width="${w}" height="${barchartSize}"></canvas>`;
+    };
+    el.style.whiteSpace = 'nowrap';
+
+    var refreshFilter = (event, ui) => {
+      $(ui.handle.parentNode).find('.ui-slider-handle').text( Math.floor(100 * (ui.value - 127) / 128.0) + '%');
+    };
+
+    var getMousePos = (canvas, evt) => {
+        var rect = canvas.getBoundingClientRect();
+        return { x: evt.clientX - rect.left
+               , y: evt.clientY - rect.top
+               };
+      };
 
     return {
 
       renderValue: function(x) {
 
         var data = x.data[0].hasOwnProperty('length') ? [].concat(... (x.data || []) ) : x.data;
-        var imageWidth  = 512
-          , imageHeight = 512
+        var maxValue = data.reduce((a, b) => (a < b ? b : a), 0);
+        var normdata = data.map((d, i) => Math.floor(255.0 * d / maxValue));
+        var imageWidth  = data.width  || 512
+          , imageHeight = data.height || 512
           ;
 
-        var maxValue = data.reduce((a, b) => (a < b ? b : a), 0);
-        var canvas = document.createElement('canvas');
+        var id = el.id;
+        // el.innerHtml = widgetInnerHtml(id, imageWidth, imageHeight);
+        $(el).append($(widgetInnerHtml(id, imageWidth, imageHeight)));
+        // return;
+        // var canvas = document.createElement('canvas');
+        var canvas = document.getElementById('image');
         var context = canvas.getContext("2d");
 
-        var barcharts = Array.from({length: 2 }, (_, i) => {
-          var barchart = document.createElement('canvas');
-          barchart.id = `barchart${i + 1}`;
-          barchart.width  = 0 === i ? 128 : imageWidth;
-          barchart.height = 0 === i ? imageHeight : 128;
-          barchart.style.border = '1px solid red';
-          return barchart;
+        var barcharts = { X: { canvas: $(el).find('#barchartX')[0] }
+                        , Y: { canvas: $(el).find('#barchartY')[0] }
+                        }
+        Object.keys(barcharts).forEach( k => {
+          barcharts[k].ctx = barcharts[k].canvas.getContext("2d");
+          // barcharts[k].ctx.fillStyle = 'red'; barcharts[k].ctx.fillRect(0, 10, 100, 100);
         });
-        var barchartContexts = barcharts.map( barchart => barchart.getContext("2d") );
+        // var barchartContexts = Object.keys(barcharts).map( k => barcharts[k].canvas.getContext("2d") );
 
         var imagedata = context.createImageData(imageWidth, imageHeight);
-        var normdata = data.map((d, i) => Math.floor(255.0 * d / maxValue));
         normdata.forEach( (c, i) => {
           imagedata.data[i * 4    ] = c;
           imagedata.data[i * 4 + 1] = c;
@@ -40,30 +69,9 @@ HTMLWidgets.widget({
           imagedata.data[i * 4 + 3] = 255;
         })
 
-        el.style.whiteSpace = 'nowrap';
-
-        var id = el.id;
         context.putImageData(imagedata, 0, 0);
-        // convert the image to a texture
-        // insert canvas into widget
-        canvas.setAttribute('id', 'image');
-        canvas.setAttribute('width', imageWidth);
-        canvas.setAttribute('height', imageHeight);
-        el.appendChild(barcharts[0]);
-        el.appendChild(canvas);
-        var slidersNode = $('<div id="sliders">');
-        slidersNode.append( $(`<div id="brightness_${id}" class="brightness slider"></div>`) );
-        slidersNode.append( $(`<div id="contrast_${id}" class="contrast slider"></div>`) );
-        $(el).append( slidersNode );
-        var outputValuesNode = $('<div id="outputValues"><span>X:</span><input type="text" size="10"/>&nbsp;<span>Y:</span><input type="text" size="10"/>&nbsp;<span>VALUE:</span><input type="text" size="10"/></div>');
-        $(el).append(outputValuesNode);
-        $(el).append(barcharts[1]);
 
-        var refreshFilter = function(event, ui) {
-          $(ui.handle.parentNode).find('.ui-slider-handle').text( Math.floor(100 * (ui.value - 127) / 128.0) + '%');
-        };
-
-        $('div.slider').slider({
+        $(el).find('div.slider').slider({
           orientation: "vertical",
           range: "min",
           max: 255,
@@ -72,18 +80,12 @@ HTMLWidgets.widget({
           change: refreshFilter
         });
 
-        $( `#brightness_${id}` ).slider( "value", 123 );
-        $( `#contrast_${id}`   ).slider( "value", 245 );
+        $(el).find( `#brightness_${id}` ).slider( "value", 123 );
+        $(el).find( `#contrast_${id}`   ).slider( "value", 245 );
 
 
         var canvasMousePos = { x: NaN, y: NaN, in: false };
-        var getMousePos = (canvas, evt) => {
-            var rect = canvas.getBoundingClientRect();
-            return { x: evt.clientX - rect.left
-                   , y: evt.clientY - rect.top
-                   };
-          };
-        var inputs = $('#outputValues input');
+        var inputs = $(el).find('#outputValues input');
         canvas.addEventListener('mouseenter', evt => { canvasMousePos.in = true; })
         canvas.addEventListener('mouseleave', evt => { canvasMousePos.in = false; })
         canvas.addEventListener('mousemove', evt => {
@@ -101,24 +103,46 @@ HTMLWidgets.widget({
             ;
           var filtered = ImageFilters.BrightnessContrastGimp(imagedata, brightness, contrast)
           context.putImageData(filtered, 0, 0);
+
           if ( canvasMousePos.in ) {
+            // cursor cross
             context.fillStyle = 'red';
             context.fillRect(0, canvasMousePos.y, imageWidth, 1);
             context.fillRect(canvasMousePos.x, 0, 1, imageHeight);
 
-            barchartContexts[1].clearRect(0, 0, barcharts[1].width, barcharts[1].height);
-            barchartContexts[1].strokeStyle = 'black';
-            barchartContexts[1].beginPath();
-            barchartContexts[1].setLineDash([5, 10]);
-            barchartContexts[1].moveTo(canvasMousePos.x, 0);
-            barchartContexts[1].lineTo(canvasMousePos.x, barcharts[1].height);
-            barchartContexts[1].stroke();
-            barchartContexts[1].fillStyle = 'green';
-            var line = normdata.filter((v, i) => Math.floor(i / imageWidth) == canvasMousePos.y)
-                      .map(v => v / 2);
-            for (var i = 0; i < imageWidth; i++) {
-              barchartContexts[1].fillRect(i, barcharts[1].height - line[i], 1, line[i]);
-            }
+            // barcharts
+            Object.keys(barcharts).forEach(k => {
+              var chart = barcharts[k].canvas
+                , ctx   = barcharts[k].ctx
+                ;
+              ctx.clearRect(0, 0, chart.width, chart.height);
+              ctx.strokeStyle = 'black';
+              ctx.beginPath();
+              ctx.setLineDash([5, 5]);
+              if ('X' === k) {
+                ctx.moveTo(canvasMousePos.x, 0);
+                ctx.lineTo(canvasMousePos.x, chart.height);
+              } else {
+                ctx.moveTo(0, canvasMousePos.y);
+                ctx.lineTo(chart.width, canvasMousePos.y);
+              }
+              ctx.stroke();
+
+              ctx.fillStyle = 'green';
+              var line = normdata.filter((v, i) => (
+                  ('X' === k)
+                  ? Math.floor(i / imageWidth) == canvasMousePos.y
+                  : (i % imageWidth) == canvasMousePos.x
+                )).map(v => v / 2);
+
+              for (var i = 0; i < imageWidth; i++) {
+                if ('X' === k) {
+                  ctx.fillRect(i, chart.height - line[i], 1, line[i]);
+                } else {
+                  ctx.fillRect(chart.width - line[i], i, line[i], 1);
+                }
+              }
+            })
           }
 
           requestAnimationFrame(animationFrame);

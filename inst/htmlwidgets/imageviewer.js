acf31f3fe8ea348696ac30c385a060de35f4d629
Iakov Pustilnik <xyapus@gmail.com>
2018-01-21 02:23:20 +0300

    use d3 for svg charts

--- inst/htmlwidgets/imageviewer.js
+++ inst/htmlwidgets/imageviewer.js
@@ -9,7 +9,7 @@ HTMLWidgets.widget({
     var barchartSize = 128;
     var widgetInnerHtml = (id, w, h) => {
       return `<div class="widgetcontainer"><div class="row row1" style="height:${h}px"><!--
-           --><canvas id="barchartY" width="${barchartSize}" height="${h}"></canvas><!--
+           --><svg id="barchartY" width="${barchartSize}" height="${h}"></svg><!--
            --><canvas id="image" width="${w}" height="${h}"></canvas><div id="sliders"><!--
            --><div id="brightness_${id}" class="brightness slider"></div><!--
            --><div id="contrast_${id}" class="contrast slider"></div><!--
@@ -20,7 +20,7 @@ HTMLWidgets.widget({
            --><ul><li>Y:</li><input type="text" size="10"/></ul><!--
            --><ul><li>VAL:</li><input type="text" size="10"/></ul><!--
            --></div><!--
-           --><canvas id="barchartX" width="${w}" height="${barchartSize}"></canvas><!--
+           --><svg id="barchartX" width="${w+100}" height="${barchartSize}"></svg><!--
            --></div></div>`;
     };
     el.style.whiteSpace = 'nowrap';
@@ -31,6 +31,182 @@ HTMLWidgets.widget({
                , y: evt.clientY - rect.top
                };
       };
+
+    var selectOrAppend = (node, selector, append = selector) => {
+      var c = node.select(selector);
+      return c.empty()
+            ? node.append(append)
+            : c
+    }
+
+    var d3_xy_chart = ({
+        width = 640
+      , height = 480
+      , xlabel = "X Axis Label"
+      , ylabel = "Y Axis Label"
+      , rotate = '0deg'
+    } = {}) => {
+
+      var chart = (selection) => {
+        selection.each(function(datasets) {
+            //
+            // Create the plot.
+            //
+            // var margin = {top: 20, right: 80, bottom: 30, left: 50},
+            // var margin = {top: 0, right: 100, bottom: 0, left: 0},
+            // var margin = {top: 0, right: 0, bottom: 0, left: 0},
+            var margin = {top: 0, right: 100, bottom: 0, left: 0},
+                innerwidth = width - margin.left - margin.right,
+                innerheight = height - margin.top - margin.bottom ;
+
+            var x_scale = d3.scale.linear()
+                .range([0, innerwidth])
+                .domain([ d3.min(datasets, d => d3.min(d.x) ),
+                          d3.max(datasets, d => d3.max(d.x) ) ]) ;
+
+            var y_scale = d3.scale.linear()
+                .range([innerheight, 0])
+                .domain([ d3.min(datasets, function(d) { return d3.min(d.y); }),
+                          d3.max(datasets, function(d) { return d3.max(d.y); }) ]) ;
+
+            var color_scale = d3.scale.category10()
+                .domain(d3.range(datasets.length)) ;
+
+            var x_axis = d3.svg.axis()
+                .scale(x_scale)
+                // .scale( d3.scale.linear().range([ d3.min(datasets, d => d3.min(d.x) ), d3.max(datasets, d => d3.max(d.x) ) ]) )
+                .ticks( Math.floor(imageWidth / 100) )
+                .tickSize(-5)
+                .orient("bottom") ;
+
+            var y_axis = d3.svg.axis()
+                .scale(y_scale)
+                // .scale( d3.scale.linear().range([ d3.min(datasets, d => d3.min(d.y) ), d3.max(datasets, d => d3.max(d.y) ) ]) )
+                // .orient("left") ;
+                .ticks(3, "e")
+                .tickSize(-5)
+                .orient("right") ;
+
+            var x_grid = d3.svg.axis()
+                .scale(x_scale)
+                .orient("bottom")
+                .ticks( Math.floor(imageWidth / 100) )
+                .tickSize(-innerheight)
+                .tickFormat("") ;
+
+            var y_grid = d3.svg.axis()
+                .scale(y_scale)
+                .orient("left")
+                .tickSize(-innerwidth)
+                .tickFormat("") ;
+
+            var draw_line = d3.svg.line()
+                .interpolate("linear")
+                .x(d => x_scale(d[0]) )
+                .y(d => y_scale(d[1]) );
+
+            var svg = d3.select(this)
+                .attr("width", width)
+                .attr("height", height);
+
+            selectOrAppend(svg, 'g')
+                .attr("transform", `translate(${margin.left},${margin.top}) rotate(${rotate})`) ;
+
+            svg = svg.select("g");
+
+            selectOrAppend(svg, "g.x.grid", "g")
+                .attr("class", "x grid")
+                .attr("transform", `translate(0, ${innerheight})`)
+                .call(x_grid);
+
+            selectOrAppend(svg, "g.y.grid", "g")
+                .attr("class", "y grid")
+                .call(y_grid) ;
+
+            selectOrAppend(
+              selectOrAppend(svg, 'g.x.axis', 'g')
+                .attr("class", "x axis")
+                .attr("transform", `translate(0, ${innerheight})`)
+                .call(x_axis)
+            , "text.label"
+            , "text"
+            ).attr("dy", "-.71em")
+             .attr("x", innerwidth)
+             .attr("class", "label")
+             .style("text-anchor", "end")
+             .text(xlabel)
+             ;
+
+            selectOrAppend(
+              selectOrAppend(svg, 'g.y.axis', 'g')
+                .attr("class", "y axis")
+                .attr("transform", `translate(${innerwidth}, 0)`)
+                .call(y_axis)
+            , "text.label"
+            , "text"
+            ).attr("transform", "rotate(-90)")
+             .attr("y", 6)
+             .attr("class", "label")
+             .attr("dy", "0.71em")
+             .style("text-anchor", "end")
+             .text(ylabel)
+             ;
+
+            var data_lines = svg.selectAll(".d3_xy_chart_line")
+                .data(datasets.map(function(d) {return d3.zip(d.x, d.y); }));
+            data_lines
+                .enter().append("g")
+                .attr("class", "d3_xy_chart_line")
+            data_lines
+                .exit().remove();
+
+            selectOrAppend(data_lines, "path")
+                .attr("class", "line")
+                .attr("d", d => draw_line(d) )
+                .attr("stroke", (_, i) => color_scale(i) );
+
+            selectOrAppend(data_lines, "text")
+                .datum((d, i) => { return {name: datasets[i].label, final: d[d.length-1]}; })
+                .attr("transform", d => (
+                    null != d && null != d.final
+                      ? ( "translate(" + x_scale(d.final[0]) + "," + y_scale(d.final[1]) + ")" )
+                      : ''
+                  ))
+                .attr("x", 3)
+                .attr("dy", ".35em")
+                .attr("fill", (_, i) => color_scale(i) )
+                .text(d => d.name );
+
+        }) ;
+      }
+
+      chart.width = (value) => {
+          if (!arguments.length) return width;
+          width = value;
+          return chart;
+      }
+
+      chart.height = (value) => {
+          if (!arguments.length) return height;
+          height = value;
+          return chart;
+      }
+
+      chart.xlabel = (value) => {
+          if(!arguments.length) return xlabel ;
+          xlabel = value ;
+          return chart ;
+      }
+
+      chart.ylabel = (value) => {
+          if(!arguments.length) return ylabel ;
+          ylabel = value ;
+          return chart ;
+      }
+
+      return chart;
+    }
+
 
     return {
 
@@ -47,18 +223,18 @@ HTMLWidgets.widget({
           , imageHeight = data.height || height
           ;
 
+        var xy_chart_wide = d3_xy_chart({ width: imageWidth + 100, height: barchartSize, xlabel: '', ylabel: '' });
+        var xy_chart_tall = d3_xy_chart({ width: imageWidth + 100, height: barchartSize, xlabel: '', ylabel: '' });
+
         var id = el.id;
         $(el).append( $(widgetInnerHtml(id, imageWidth, imageHeight)) );
 
         var canvas = document.getElementById('image');
         var context = canvas.getContext("2d");
 
-        var barcharts = { X: { canvas: $(el).find('#barchartX')[0] }
-                        , Y: { canvas: $(el).find('#barchartY')[0] }
+        var barcharts = { X: { svg: d3.select( $(el).find('#barchartX')[0] ) }
+                        , Y: { svg: d3.select( $(el).find('#barchartY')[0] ) }
                         }
-        Object.keys(barcharts).forEach( k =>
-          barcharts[k].ctx = barcharts[k].canvas.getContext("2d")
-        );
 
         var imagedata = context.createImageData(imageWidth, imageHeight);
         normdata.forEach( (c, i) => {
@@ -126,51 +302,25 @@ HTMLWidgets.widget({
 
             // barcharts
             Object.keys(barcharts).forEach(k => {
-              var chart = barcharts[k].canvas
-                , ctx   = barcharts[k].ctx
-                ;
-              ctx.clearRect(0, 0, chart.width, chart.height);
-              ctx.strokeStyle = 'black';
-              ctx.beginPath();
-              ctx.setLineDash([5, 5]);
-              if ('X' === k) {
-                ctx.moveTo(canvasMousePos.x, 0           );
-                ctx.lineTo(canvasMousePos.x, chart.height);
-              } else {
-                ctx.moveTo(0,           canvasMousePos.y);
-                ctx.lineTo(chart.width, canvasMousePos.y);
-              }
-              ctx.stroke();
-
-              if ( isBarChart ) {
-                ctx.fillStyle = 'green';
-              } else {
-                ctx.strokeStyle = 'green';
-                ctx.beginPath();
-                ctx.setLineDash([]);
-                ctx.moveTo(...( ('X' === k)
-                            ? [ 0, chart.height ]
-                            : [ chart.width, 0 ]
-                              ) );
-              }
-
-              var line = normdata.filter((v, i) => (
+              var svg = barcharts[k].svg;
+              var line = data.filter((v, i) => (
                   ('X' === k)
                   ? Math.floor(i / imageWidth) == canvasMousePos.y
                   : (i % imageWidth) == canvasMousePos.x
-                )).map(v => v / 2);
-
-              for (var i = 0; i < imageWidth; i++) {
-                if ('X' === k) {
-                  if ( isBarChart ) ctx.fillRect(i, chart.height - line[i], 1, line[i]);
-                  else              ctx.lineTo(i, chart.height - line[i]);
-                } else {
-                  if ( isBarChart ) ctx.fillRect(chart.width - line[i], i, line[i], 1);
-                  else              ctx.lineTo(chart.width - line[i], i);
-                }
+                )); // .map(v => v / 2);
+              if ('X' === k) {
+                svg.data([[{
+                  label: '' // `x = ${canvasMousePos.x}`
+                , x    : Array.from({ length: imageWidth }, (_, i) => i)
+                , y    : line
+                }]]).call(xy_chart_wide);
+              } else {
+                svg.data([[{
+                  label: '' // `x = ${canvasMousePos.x}`
+                , x    : Array.from({ length: imageWidth }, (_, i) => i)
+                , y    : line.reverse()
+                }]]).call(xy_chart_tall);
               }
-              // if ( !isBarChart )
-                ctx.stroke();
             })
           }
           isUpdated = false;
